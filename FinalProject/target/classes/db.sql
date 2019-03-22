99ec61600bac786b131a6ffea10b30717025af0b
soldesk <soldesk@soldesk-PC>
2019-03-22 11:55:09 +0900

    달력에서 타이틀 클릭시 상세메뉴로 가게 메인페이지 css display수정했음

--- FinalProject/target/classes/db.sql
+++ FinalProject/target/classes/db.sql
@@ -2,3 +2,5 @@ select * from GOV_EVENT order by ge_area_code;
 
 select * from gov_event where ge_title like '%'||'서귀포'||'%' order by ge_area_code
 select * from gov_event
+
+select * from gov_event where ge_title like '%'||'서대문, 1919 그날의 함성!'||'%';
